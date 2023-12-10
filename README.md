f7ea56c73acb111da1deaac37919f391e24c6c75
Frederick Boehm <frederick.boehm@gmail.com>
2023-12-10 10:46:59 -0500

    fix: added notes for bolding my name in refs

--- README.md
+++ README.md
@@ -7,5 +7,11 @@ if working in RStudio, be sure to compile by using Makefile, rather than by usin
 https://github.com/mps9506/quarto-cv offers the `quarto-cv.pdf` format for quarto output.
 
 
+## bolding my name in output pdf
+
+https://tex.stackexchange.com/questions/33330/make-one-authors-name-bold-every-time-it-shows-up-in-the-bibliography
+
+https://stackoverflow.com/questions/56873622/highlight-one-specific-author-when-generating-references-in-pandoc
+
 
 
--- parsing-bib-files/split_bib.py
+++ parsing-bib-files/split_bib.py
@@ -1,7 +1,10 @@
+import configargparse
+from pybtex.database import BibliographyData
 from pybtex.database.input import bibtex
 from pybtex.database.output import bibtex as bib_out
-from pybtex.database import BibliographyData
 
+def get_entry_types(entries):
+    return set(entry.type for entry in entries.values())
 
 def filter_entries(entries, entry_type):
     return {key: entry for key, entry in entries.items() if entry.type == entry_type}
@@ -12,16 +15,23 @@ def save_to_file(entries, filename):
         writer = bib_out.Writer()
         writer.write_stream(bib_data, bibfile)
 
-def main():
-    with open('../boehm.bib', 'r', encoding='utf-8') as bibfile:
+def main(args):
+    with open(args.input_file, 'r', encoding='utf-8') as bibfile:
         parser = bibtex.Parser()
         bib_data = parser.parse_file(bibfile)
 
-        for entry_type in ['article', 'book', 'incollection', 'software', 'inproceedings', 'misc', 'unpublished']:
+        entry_types = get_entry_types(bib_data.entries)
+
+        for entry_type in entry_types:
             filtered_entries = filter_entries(bib_data.entries, entry_type)
-            output_filename = f'boehm-{entry_type}.bib'
+            output_filename = f'{entry_type}.bib'
             save_to_file(filtered_entries, output_filename)
             print(f'{output_filename} created successfully.')
 
 if __name__ == "__main__":
-    main()
+    parser = configargparse.ArgParser(description='Split BibTeX file based on entry types.')
+    parser.add_argument('input_file', help='Path to the original BibTeX file', metavar='FILE')
+
+    args = parser.parse_args()
+
+    main(args)
